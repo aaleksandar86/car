6c9810eebfbedb2ae24cc7e5167e269d06bacd9c
Aleksandar Apostolov <a_aleksandar86@yahoo.com>
2020-03-05 06:15:12 +0100

    Speed up method implemented with tests in rspec

--- lib/car.rb
+++ lib/car.rb
@@ -4,12 +4,17 @@ module Car
   class MyCar
     attr_accessor :color, :car_speed
     attr_reader :year
+    SPEED_UP_RATE = 10
 
     def initialize(model, year, color)
       @model = model
       @year = year
       @color = color
       @car_speed = 0
+    end
+
+    def speed_up
+      @car_speed += SPEED_UP_RATE
     end
   end
 end
--- spec/mycar_spec.rb
+++ spec/mycar_spec.rb
@@ -5,7 +5,16 @@ RSpec.describe Car::MyCar do
         expect(volvo).to be_instance_of(Car::MyCar)
     end
 
-    it"has initial speed 0" do
+    it "has initial speed 0" do
         expect(volvo.car_speed).to eq(0)
+    end
+
+    it "doesn't have negative value for speed" do
+        expect(volvo.car_speed).to be >= 0
+    end
+
+    it "has a speed up rate of 10" do
+        volvo.speed_up #call function once
+        expect(volvo.car_speed).to be >= 10
     end
 end
