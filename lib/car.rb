3586109d1e4bd1ce09875d84529b4ed9d5363f7b
Aleksandar Apostolov <a_aleksandar86@yahoo.com>
2020-03-07 13:38:51 +0100

    add_defect(defect) and service_info functions added in MyCar class

--- lib/car.rb
+++ lib/car.rb
@@ -1,21 +1,40 @@
-require "car/version"
+require 'car/version'
 
 module Car
+  # creating a class car
   class MyCar
+    SPEED_OFFSET = 15
+
     attr_accessor :color, :car_speed
-    attr_reader :year
-    SPEED_UP_RATE = 10
+    attr_reader :year, :defects
 
     def initialize(model, year, color)
       @model = model
       @year = year
       @color = color
       @car_speed = 0
+      @defects = []
     end
 
-    def speed_up
-      @car_speed += SPEED_UP_RATE
+    def speed_up(speed = SPEED_OFFSET)
+      @car_speed += speed
+    end
+
+    def slow_down(decrease = SPEED_OFFSET)
+      @car_speed -= decrease
+    end
+
+    def spray_paint(arg)
+      @color = arg
+    end
+
+    def add_defect(defect)
+      @defects.push(defect)
+    end
+
+    def service_info
+      defects = @defects.join(', ')
+      "Defects to be fixed: #{defects}"
     end
   end
 end
-
